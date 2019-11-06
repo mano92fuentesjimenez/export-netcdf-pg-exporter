1d78c44cd9a4ae82492977f8baa1291325687a64
miggy92 <m92fuentes@gmail.com>
2019-11-06 14:28:35 -0500

    add test case

--- tests/PgExporter.js
+++ tests/PgExporter.js
@@ -47,4 +47,43 @@ describe('PgExporter', () => {
       ['insert into "public"."test" values  ( \'srid=4326;point(5 5)\')', []],
     );
   });
+
+  it('exports a table with two columns one geom column and another float column', async () => {
+    const pgPool = new Pool();
+    const pgQueryStub = sandbox.stub(pgPool, 'query');
+
+    const exporter = pgExporter({
+      variableUses: { LATITUDE: 'lat', LONGITUDE: 'lon' },
+      createTable: true,
+      schema: 'public',
+      table: 'test',
+      pgPool,
+    });
+
+    await exporter.init([
+      {
+        fieldName: 'lat',
+        fieldType: 'float',
+      },
+      {
+        fieldName: 'lon',
+        fieldType: 'float',
+      },
+      {
+        fieldName: 'number',
+        fieldType: 'float',
+      },
+    ], 1);
+
+    exporter.write([5, 5, 20]);
+
+    await exporter.finishWriting();
+
+    expect(pgQueryStub.callCount).to.equal(2);
+
+    expect(pgQueryStub.firstCall.args).to.deep.equal(['create table "public"."test"( geom geometry(point, 4326), number numeric );']);
+    expect(pgQueryStub.secondCall.args).to.deep.equal(
+      ['insert into "public"."test" values  ( \'srid=4326;point(5 5)\', $0)', [20]],
+    );
+  });
 });
